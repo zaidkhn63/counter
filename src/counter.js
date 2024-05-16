7b5a13cc9e0fc0ff6f0d708f86a32d47980bb714
Ameena Khanum <ameenakhanum@Ameenas-MacBook-Pro.local>
2024-05-16 19:20:48 +0530

    counter

--- src/counter.js
+++ src/counter.js
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
 
-  const handleIncrement = () => {
-    // Increase count by 1 on each click
-    setCount((prevCount) => prevCount + 1);
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
   };
 
-  const handleDecrement = () => {
-    // Decrease count by 1 on each click
-    setCount((prevCount) => prevCount - 1);
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1
+    }));
   };
 
-  return (
-    <div className="counter">
-      <h2>Counter App</h2>
-      <p>Count: {count}</p>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
-    </div>
-  );
-};
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="counter">
+        <h2>Counter App</h2>
+        <p>Count: {count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
